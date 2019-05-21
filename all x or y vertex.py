6a49878f0f6269d3bde69310e3174bba5406584b
Guan_lin <40513344+guainlin@users.noreply.github.com>
2019-05-21 21:16:15 +0800

    Add files via upload

---
+++ all x or y vertex.py
@@ -0,0 +1,11 @@
+'all x or y vertex'
+def li_x_y (vertexes):
+    li_x = [vertex[0] for vertex in vertexes]
+    li_y = [vertex[1] for vertex in vertexes]    
+    return (li_x , li_y)
+
+'''
+Ex:
+li_x_y ([[1,4],[5,2],[6,7]])
+
+'''
